e54deb575691819f9b841341ab7356d1340a3ec8
Johanna Honma <johonma@hotmail.com>
2018-02-19 22:41:12 -0700

    burger

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Spicy Schicken Sandwich');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Avocado Burger', true);
